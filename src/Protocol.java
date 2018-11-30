edac4d0ab8717315a7cc7b40779fdde88c962f8b
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-11-30 09:54:36 +0000

    (minor) Just reformatted the code and removed a redundant comment. Nothing to see here ..

--- src/Protocol.java
+++ src/Protocol.java
@@ -32,9 +32,9 @@ public class Protocol {
     public boolean run() {
         System.out.println("============ INITIALISATION FINISHED.START OF THE ACTUAL PART OF THE PROTOCOL ============");
         System.out.println("Step 1: Alice asks server for Bob's public key");
-        Map<String,int[]>  serverResponse= askServerForAPublicKey();
+        Map<String, int[]> serverResponse = askServerForAPublicKey();
         System.out.println("Step 2: Server sent Alice a signed version of Bob's public key");
-        if (!isMessageReallyFromServer(serverResponse)){
+        if (!isMessageReallyFromServer(serverResponse)) {
             System.out.println("The message has been intercepted! Digital signature from server doesn't match the original");
             System.out.println("This protocol instance has been compromised, aborting the protocol. Try running the protocol again");
             return false;
@@ -42,41 +42,41 @@ public class Protocol {
         int[] bobsPublicKey = serverResponse.get("unsignedPublicKey");
         System.out.println("Step 3.1: Alice creates her nonce and encrypts it using Bob's public key from the Trusted server");
         this.aliceNonce = createNonce(bobsPublicKey);
-        this.aliceEncryptedNonce = RSAEncrypt(this.aliceNonce,bobsPublicKey[0], bobsPublicKey[1]);
+        this.aliceEncryptedNonce = RSAEncrypt(this.aliceNonce, bobsPublicKey[0], bobsPublicKey[1]);
         System.out.println("Step 3.2: Alice sends Bob her nonce..");
 //        ...
         System.out.println("Step 4: Bob asks server for Alice's public key");
-        Map<String,int[]>  serverResponse2= askServerForAPublicKey();
-        if (!isMessageReallyFromServer(serverResponse2)){
+        Map<String, int[]> serverResponse2 = askServerForAPublicKey();
         System.out.println("Step 5: (if you asked server for 'ALICE'): Server sent Bob a signed version of Alice's public key");
+        if (!isMessageReallyFromServer(serverResponse2)) {
             System.out.println("The message has been intercepted! Digital signature from server doesn't match the original");
             System.out.println("This protocol instance has been compromised, aborting the protocol. Try running the protocol again");
             return false;
         }
         int[] alicesPublicKey = serverResponse2.get("unsignedPublicKey");
         System.out.println("Step 6.1: Bob encrypts his nonce using Alice's public key from the Trusted server");
-        this.bobNonce= createNonce(bobsPublicKey);
-        this.bobEncryptedNonce= RSAEncrypt(this.aliceNonce,alicesPublicKey[0], alicesPublicKey[1]);
+        this.bobNonce = createNonce(bobsPublicKey);
+        this.bobEncryptedNonce = RSAEncrypt(this.bobNonce, alicesPublicKey[0], alicesPublicKey[1]);
         System.out.println("Step 6.2: Bob decrypts Alice's nonce using his private key");
         this.aliceNonceDecryptedByBob = Bob.RSAIntDecrypt(this.aliceEncryptedNonce);
-        if (this.aliceNonceDecryptedByBob != this.aliceNonce){
+        if (this.aliceNonceDecryptedByBob != this.aliceNonce) {
             System.out.println("ERROR! ALICE'S ORIGINAL NONCE DOESN'T MATCH WITH WHAT BOB HAS DECRYPTED!");
             System.out.println("Alice's original nonce: " + this.aliceNonce);
             System.out.println("Bob's decrypted nonce: " + this.aliceNonceDecryptedByBob);
             return false;
-        }else {
+        } else {
             System.out.println("SUCCESS! WHAT BOB HAS DECRYPTED WITH HIS PRIVATE KEY MATCHES ALICE'S NONCE");
         }
         System.out.println("Step 6.3: Bob sends Alice her decrypted nonce and his encrypted nonce");
 //        ...
         System.out.println("Step 7.1: Alice decrypts Bob's nonce using her private key");
         this.bobNonceDecryptedByAlice = Alice.RSAIntDecrypt(this.bobEncryptedNonce);
-        if (this.bobNonceDecryptedByAlice  != this.bobNonce){
+        if (this.bobNonceDecryptedByAlice != this.bobNonce) {
             System.out.println("ERROR! BOB'S ORIGINAL NONCE DOESN'T MATCH WITH WHAT ALICE HAS DECRYPTED!");
             System.out.println("Bob's original nonce: " + this.bobNonce);
             System.out.println("Alice's decrypted nonce: " + this.bobNonceDecryptedByAlice);
             return false;
-        }else {
+        } else {
             System.out.println("SUCCESS! WHAT BOB HAS DECRYPTED WITH HIS PRIVATE KEY MATCHES ALICE'S NONCE");
         }
         System.out.println("Step 7.2: Alice sends Bob's decrypted nonce ");
@@ -85,14 +85,14 @@ public class Protocol {
         return true;
     }
 
-    public int createNonce (int[] publicKey){
+    public int createNonce(int[] publicKey) {
         Scanner s = new Scanner(System.in);
         System.out.println("Please provide a nonce to be encrypted.");
         System.out.println("(NOTE: This algorithm is an academic example and thus for an input it only accepts INTEGER values that are LESS THAN 'N')");
         System.out.println("Please type in the number you want to be a message: ");
-        while (true){
+        while (true) {
             int message = s.nextInt();
-            if (message <  publicKey[1]){
+            if (message < publicKey[1]) {
                 return message;
             }
             System.out.println("Error! Please provide a correct int smaller than N as the nonce");
@@ -104,7 +104,7 @@ public class Protocol {
         return rsaNonNaiveEncryption .result;
     }
 
-    public boolean isMessageReallyFromServer(Map<String,int[]> serverResponse){
+    public boolean isMessageReallyFromServer(Map<String, int[]> serverResponse) {
         System.out.println("Comparing the signed public key with the original one ...");
         int[] signedPK = serverResponse.get("signedPublicKey");
         int[] originalPK = serverResponse.get("unsignedPublicKey");
@@ -112,7 +112,7 @@ public class Protocol {
         candidatePK[0] = this.Server.unSignMessage(signedPK[0]);
         candidatePK[1] = this.Server.unSignMessage(signedPK[1]);
         for (int i = 0; i < 2; i++) {
-            if (candidatePK[i] != originalPK[i]){
+            if (candidatePK[i] != originalPK[i]) {
                 System.out.println("At index:" + i + ": candidatePK doesn't match original pk !");
                 System.out.println("Candidate PK value: " + candidatePK[i]);
                 System.out.println("Original PK value: " + originalPK[i]);
@@ -124,7 +124,7 @@ public class Protocol {
         return true;
     }
 
-    public Map<String,int[]>  askServerForAPublicKey() {
+    public Map<String, int[]> askServerForAPublicKey() {
         Scanner s = new Scanner(System.in);
         System.out.println("[SERVER]: WHO'S PUBLIC KEY SHALL I SEND YOU? (Type 'ALICE' or 'BOB')");
         while (true) {
@@ -167,7 +167,7 @@ public class Protocol {
             }
         }
 
-        public Map<String,int[]>  sendPublicKey(String whosePublicKey) {
+        public Map<String, int[]> sendPublicKey(String whosePublicKey) {
             int[] publicKey = new int[2];
             switch (whosePublicKey) {
                 case "ALICE":
@@ -207,7 +207,6 @@ public class Protocol {
          * Finally, it performs Rabin-Miller Primality tests on both p & q
          * To ensure almost ~100% guarantee both are prime numbers.
          */
-//    TODO: MUST CHECK IF THIS WORKS !
         private void drawRandomPAndQ(MillerRabin mr) {
             boolean pAndQFarAway = false;
             boolean pIsPrime = false;
